152d51df74e445991763a207c6b14dad9a5b67c0
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-11-19 15:37:40 -0600

    transfered

---
+++ script.js
@@ -0,0 +1,42 @@
+var myApp = angular.module('GoodLookingApp', []);
+
+ myApp.controller('calculatorCtrl', function($scope) {
+   
+   
+
+   function add(x, y) {
+     return x + y;
+   }
+
+   function sub(x, y) {
+     return x - y;
+   }
+
+   function div(x,y) {
+     return x / y;
+   }
+
+   function multiply(x,y) {
+     return x * y;
+   }
+  
+   function modulus(x,y) {
+     return x % y;
+   }
+
+   function calc(op, x, y) {
+     return $scope.operators[op](parseInt(x), parseInt(y));
+   }
+
+   $scope.operators = {
+     '+': add,
+     '-': sub,
+     '/': div,
+     '*': multiply,
+     '%': modulus,
+     
+   };
+   $scope.op = '+';
+   $scope.calc = calc;
+
+ });

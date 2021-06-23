a54518a7227f306e3b330e6ab3932aeb284ae90b
Charles Butler <cdbutle@outlook.com>
2021-06-22 19:00:10 -0500

    creating new variables

--- script.js
+++ script.js
@@ -1,73 +1,45 @@
 var app = angular.module('GoodLookingCal', []);
 
  app.controller('calculatorCtrl', function($scope) {
-   
-   
 
-   function add(x, y) {
-     return x + y;
-   }
+  //  function add(x, y) {
+  //    return x + y;
+  //  }
 
-   function sub(x, y) {
-     return x - y;
-   }
+  //  function sub(x, y) {
+  //    return x - y;
+  //  }
 
-   function div(x,y) {
-     return x / y;
-   }
+  //  function div(x,y) {
+  //    return x / y;
+  //  }
 
-   function multiply(x,y) {
-     return x * y;
-   }
+  //  function multiply(x,y) {
+  //    return x * y;
+  //  }
   
-   function modulus(x,y) {
-     return x % y;
-   }
+  //  function modulus(x,y) {
+  //    return x % y;
+  //  }
 
-   function calc(op, x, y) {
-     return $scope.operators[op](parseInt(x), parseInt(y));
-   }
+  //  function calc(op, x, y) {
+  //    return $scope.operators[op](parseInt(x), parseInt(y));
+  //  }
 
-   $scope.operators = {
-     '+': add,
-     '-': sub,
-     '/': div,
-     '*': multiply,
-     '%': modulus,
+  //  $scope.operators = {
+  //    '+': add,
+  //    '-': sub,
+  //    '/': div,
+  //    '*': multiply,
+  //    '%': modulus,
      
-   };
-     $scope.numArr = [];
-     $scope.firstValue = "";
-     $scope.secondValue = "";
-     $scope.currentOperator = "";
-     var valueFlag = false;
-
-   $scope.op = '+';
-   $scope.calc = calc;
-
-
-   function currentValueToShow(num) {
-     assignValues(num);
-   }
-
-   function assignValues(num) {
-     if (valueFlag) {
-       $scope.secondValue += num;
-     } else {
-       $scope.firstValue += num;
-     }
-   }
-
-   $scope.showNumber = currentValueToShow;
-
-   function activateFlag() {
-     valueFlag = true;
-   }
-
-   function setCurrentOperator(operator) {
-     activateFlag();
-     $scope.currentOperator= operator;
-   }
-
-   $scope.makeSecondValue = setCurrentOperator;
+  //  };
+   
+      $scope.numArr = [];
+      $scope.modifiers = ["+", "-", "*", "/"];
+      $scope.firstValue = "";
+      $scope.secondValue = "";
+      $scope.currentModifier = "";
+      var valueFlag = false;
+    
  });

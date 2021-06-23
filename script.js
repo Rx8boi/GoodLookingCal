956f6c79ae530de8687bcc2c4a0dc63526f2204f
Charles Butler <cdbutle@outlook.com>
2021-06-22 19:10:54 -0500

    math evaluation works

--- script.js
+++ script.js
@@ -42,4 +42,42 @@ var app = angular.module('GoodLookingCal', []);
       $scope.currentModifier = "";
       var valueFlag = false;
     
+   for (var i = 9; i >= 0; i--) {
+     $scope.numArr.push(i);
+   }
+
+   function currentValueToShow(num) {
+     assignValues(num);
+   }
+
+   function assignValues(num) {
+     if (valueFlag) {
+       $scope.secondValue += num;
+     } else {
+       $scope.firstValue += num;
+     }
+   }
+
+   $scope.showNumber = currentValueToShow;
+
+   function activateFlag() {
+     valueFlag = true;
+   }
+
+   function setCurrentModifier(modifier) {
+     activateFlag();
+     $scope.currentModifier = modifier;
+   }
+
+   $scope.makeSecondValue = setCurrentModifier;
+
+   function evalMath() {
+     if ($scope.firstValue != "" && $scope.secondValue != "") {
+       var mathFormat =
+         $scope.firstValue + $scope.currentModifier + $scope.secondValue;
+       $scope.result = eval(mathFormat);
+     }
+   }
+
+   $scope.doMath = evalMath;
  });
